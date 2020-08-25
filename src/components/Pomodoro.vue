32168f0bb2797b954aac112e89d3172d8d0ac0c0
Dmitry Pashkevich <dima-vanjuve@mail.ru>
2020-08-25 23:39:01 +0300

    improve timer

--- src/components/Pomodoro.vue
+++ src/components/Pomodoro.vue
@@ -1,42 +1,50 @@
 <template>
-  <v-card class="mt-8"
-    ><v-tabs v-model="timerType" grow
-      ><v-tab v-for="tab in tabsTitle" :key="tab">{{ tab }} </v-tab>
-      <v-tabs-items v-model="timerType">
-        <v-tab-item
-          ><v-card
-            class="pa-5 d-flex flex-column align-center"
-            color="basil"
-            flat
-          >
-            <h1 class="time">{{ displayMinutes }}:{{ displaySeconds }}</h1>
-            <div class="button-group">
-              <v-btn @click="start" color="primary"
-                ><v-icon left small>mdi-play-circle-outline</v-icon>Start</v-btn
-              >
-              <v-btn @click="stop" color="error"
-                ><v-icon left small>mdi-stop-circle-outline</v-icon>Stop</v-btn
-              >
-              <v-btn @click="reset" :disabled="isRunning"
-                ><v-icon left small>mdi-restart</v-icon>Reset</v-btn
-              >
-            </div>
-          </v-card>
-        </v-tab-item>
-      </v-tabs-items>
+  <v-card class="mt-8">
+    <v-tabs @change="changeTimerTab" v-model="currentTimer" grow>
+      <v-tab v-for="tab in tabsTitle" :key="tab.name">{{ tab.name }}</v-tab>
     </v-tabs>
+    <v-card class="pa-5 d-flex flex-column align-center" color="basil" flat>
+      <h1 class="time">{{ displayMinutes }}:{{ displaySeconds }}</h1>
+      <div class="button-group">
+        <v-btn @click="start" color="primary">
+          <v-icon left small>mdi-play-circle-outline</v-icon>Start
+        </v-btn>
+        <v-btn @click="stop" color="error">
+          <v-icon left small>mdi-stop-circle-outline</v-icon>Stop
+        </v-btn>
+        <v-btn @click="reset(tabsTitle[currentTimer].minutes)" :disabled="isRunning">
+          <v-icon left small>mdi-restart</v-icon>Reset
+        </v-btn>
+      </div>
+    </v-card>
+    <SettingsDialog
+      :dialog="dialog"
+      :closeDialog="closeDialog"
+      :save="save"
+      :tabsTitle="tabsTitle"
+    />
+    <v-btn @click="dialog=true" color="secondary" dark small absolute bottom right fab>
+      <v-icon>mdi-cog-outline</v-icon>
+    </v-btn>
   </v-card>
 </template>
 
 <script>
+import SettingsDialog from "./SettingsDialog";
 export default {
+  components: { SettingsDialog },
   data() {
     return {
       isRunning: false,
-      timerType: 0,
+      currentTimer: 0,
       timerInstance: null,
       totalSeconds: 1500,
-      tabsTitle: ["Pomodoro", "Short Break", "Long Break"],
+      tabsTitle: [
+        { name: "Pomodoro", minutes: 25 },
+        { name: "Short Break", minutes: 5 },
+        { name: "Long Break", minutes: 10 },
+      ],
+      dialog: false,
     };
   },
   computed: {
@@ -61,15 +69,33 @@ export default {
       this.isRunning = true;
       this.timerInstance = setInterval(() => {
         this.totalSeconds -= 1;
+        if (this.totalSeconds <= 0) {
+          this.stop();
+          return;
+        }
       }, 1000);
     },
     stop() {
       this.isRunning = false;
       clearInterval(this.timerInstance);
     },
-    reset() {
+    reset(minutes) {
       this.stop();
-      this.totalSeconds = 1500;
+      this.totalSeconds = minutes * 60;
+    },
+    changeTimerTab(num) {
+      console.log(num);
+      this.currentTimer = num;
+      this.reset(this.tabsTitle[num].minutes);
+    },
+    closeDialog() {
+      this.dialog = false;
+    },
+    save(updatedTimers) {
+      this.tabsTitle = this.tabsTitle.map((timer, i) => {
+        return { ...timer, minutes: parseInt(updatedTimers[i]) };
+      });
+      this.closeDialog();
     },
   },
 };
@@ -77,11 +103,11 @@ export default {
 
 <style lang="sass" scoped>
 .v-card
-    width: 600px
+  width: 600px
 .time
-    font-size: 80px
-    font-weight: normal
-    text-align: center
+  font-size: 80px
+  font-weight: normal
+  text-align: center
 .v-btn
-    margin: 0 3px
+  margin: 0 3px
 </style>
---
+++ src/components/SettingsDialog.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-dialog persistent v-model="dialog" max-width="600px">
+    <v-card>
+      <v-card-title>
+        <span class="headline">Settings</span>
+      </v-card-title>
+      <v-card-text>
+        <v-text-field
+          type="number"
+          v-for="(timer,i) in updatedTimers"
+          :key="i"
+          v-model="updatedTimers[i]"
+          label="Pomodoro"
+        ></v-text-field>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="closeDialog">Close</v-btn>
+        <v-btn color="blue darken-1" text @click="save(updatedTimers)">Save</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    dialog: {
+      type: Boolean,
+      required: true,
+    },
+    closeDialog: {
+      type: Function,
+      required: true,
+    },
+    save: {
+      type: Function,
+      required: true,
+    },
+    tabsTitle: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      updatedTimers: [],
+    };
+  },
+  mounted() {
+    this.updatedTimers = this.tabsTitle.map((timer) => {
+      return timer.minutes;
+    });
+  },
+};
+</script>
+
+<style>
+</style>template>
